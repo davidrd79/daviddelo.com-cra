766b2dc3c702c683cef4ea208094b99eb136fad6
David Delo <dave@daviddelo.com>
2018-01-10 16:18:00 -0800

    Fix server code

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 
 const createApp = () => {
   const app = express();
-
-  // Static file setup
-  app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
